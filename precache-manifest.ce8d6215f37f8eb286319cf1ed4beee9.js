946bae86888a7d1f8a28ae4bf5932eb6d1c3da16
Srishti Nath <srishti.nath@gmail.com>
2020-08-21 18:06:22 -0400

    Updates

---
+++ precache-manifest.ce8d6215f37f8eb286319cf1ed4beee9.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f415c2cf3170a6bc85a279bef3ca48",
+    "url": "/hatshepsut_frontend/index.html"
+  },
+  {
+    "revision": "8fb172bd749004704f6d",
+    "url": "/hatshepsut_frontend/static/css/2.9b633cd0.chunk.css"
+  },
+  {
+    "revision": "89eb64729f902fbe8a61",
+    "url": "/hatshepsut_frontend/static/css/main.b2f999cd.chunk.css"
+  },
+  {
+    "revision": "8fb172bd749004704f6d",
+    "url": "/hatshepsut_frontend/static/js/2.93d507b9.chunk.js"
+  },
+  {
+    "revision": "3e5ac10e7701c88cc1476acc3139961e",
+    "url": "/hatshepsut_frontend/static/js/2.93d507b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89eb64729f902fbe8a61",
+    "url": "/hatshepsut_frontend/static/js/main.9ded1b87.chunk.js"
+  },
+  {
+    "revision": "1678923fe968138a7c45",
+    "url": "/hatshepsut_frontend/static/js/runtime-main.cc4a120d.js"
+  },
+  {
+    "revision": "826cf58543d5422e4645447c2fcab9fb",
+    "url": "/hatshepsut_frontend/static/media/GrimReaper.826cf585.png"
+  },
+  {
+    "revision": "901421c10dbbb4db2799fc92f648e72f",
+    "url": "/hatshepsut_frontend/static/media/Room1Lantern.901421c1.png"
+  },
+  {
+    "revision": "1802c80e5cef5046844bf5738bf72b67",
+    "url": "/hatshepsut_frontend/static/media/Room1WallInset.1802c80e.png"
+  },
+  {
+    "revision": "e065e669460b664b9076def6112b00ce",
+    "url": "/hatshepsut_frontend/static/media/Room5Jar.e065e669.png"
+  },
+  {
+    "revision": "ed250bbffbca178cbbc8eff4af297d75",
+    "url": "/hatshepsut_frontend/static/media/Room5Sand.ed250bbf.png"
+  },
+  {
+    "revision": "cd17d816e6be00da3c87d1ad72869b74",
+    "url": "/hatshepsut_frontend/static/media/Room7Statue.cd17d816.png"
+  },
+  {
+    "revision": "cae028077be2553911d1deff8da9022a",
+    "url": "/hatshepsut_frontend/static/media/Room8CanopicJar.cae02807.png"
+  },
+  {
+    "revision": "3e97e4048f1ff248e567806d242ebed4",
+    "url": "/hatshepsut_frontend/static/media/Room8Vase.3e97e404.png"
+  },
+  {
+    "revision": "d72ade6bc8e76a16064c42709634f41f",
+    "url": "/hatshepsut_frontend/static/media/Slider0.d72ade6b.png"
+  },
+  {
+    "revision": "bb5cf3003e68fba29b9b0b92b738e155",
+    "url": "/hatshepsut_frontend/static/media/Slider1.bb5cf300.png"
+  },
+  {
+    "revision": "121bc14a3c989cf2d36054dac0ce1c29",
+    "url": "/hatshepsut_frontend/static/media/Slider2.121bc14a.png"
+  },
+  {
+    "revision": "0220c472f1290eaf1870ccfe8db64961",
+    "url": "/hatshepsut_frontend/static/media/Slider3.0220c472.png"
+  },
+  {
+    "revision": "235de23c4aeae8cb411ba951a61f6557",
+    "url": "/hatshepsut_frontend/static/media/Slider4.235de23c.png"
+  },
+  {
+    "revision": "e5ac9150d71bba0e8e38ea84f3fa63f1",
+    "url": "/hatshepsut_frontend/static/media/Slider5.e5ac9150.png"
+  },
+  {
+    "revision": "aff708a0a9ada30e21edefebc65a0482",
+    "url": "/hatshepsut_frontend/static/media/Slider6.aff708a0.png"
+  },
+  {
+    "revision": "8edfbd9d07731d2b9cf84b22f3d4a854",
+    "url": "/hatshepsut_frontend/static/media/Slider7.8edfbd9d.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hatshepsut_frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hatshepsut_frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hatshepsut_frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hatshepsut_frontend/static/media/slick.f97e3bbf.svg"
+  }
+]);
