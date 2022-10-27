70a32b1c51ac6a6ca4ebaff91ce41bd549c1df1c
Srishti Nath <62907285+srishtinath@users.noreply.github.com>
2022-10-27 12:48:32 -0400

    resolve link errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,133 +1,144 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import Zoom from 'react-reveal/Zoom';
-import { withRouter, Switch, Route } from 'react-router';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Zoom from "react-reveal/Zoom";
+import { withRouter, Switch, Route } from "react-router";
 
-import FirstRoom from './FirstRoom'
-import Setting from './Setting'
-import {Rooms} from './Rooms'
-import GuessCulprit from './GuessCulprit'
-import Hatshepsut from './Hatshepsut'
-import Items from './Items'
+import FirstRoom from "./FirstRoom";
+import Setting from "./Setting";
+import { Rooms } from "./Rooms";
+import GuessCulprit from "./GuessCulprit";
+import Hatshepsut from "./Hatshepsut";
+import Items from "./Items";
 
 class Home extends Component {
-    state = {
-        showItems: false,
-        clickCount: 0
-    }
-    componentDidUpdate = (prevProps) => {
-        if (prevProps.userRooms.length !== this.props.userRooms.length){
-            if (this.props.userRooms.length === 2){
-                this.setState({
-                    showDream: true
-                })
-            } else {
-                this.setState({
-                    showDream: false
-                })
-            }
-        } 
-    }
-
-    toggleItems = (e) => {
+  state = {
+    showItems: false,
+    clickCount: 0,
+  };
+  componentDidUpdate = (prevProps) => {
+    if (prevProps.userRooms.length !== this.props.userRooms.length) {
+      if (this.props.userRooms.length === 2) {
         this.setState({
-            showItems: !this.state.showItems
-        })
-    }
-
-    renderHome = () => {
-        if (this.props.currentUser){
-            return (
-                <>
-                <div className="home-content">
-                    <Zoom>
-                        <h2>Welcome {this.props.currentUser.name}!</h2>
-                    </Zoom>
-                        <button onClick={this.seeIntroduction}>Re-read the introduction</button>
-                        <button onClick={this.continueStory}>Continue where you left off</button>
-                        <button onClick={this.guessCulpritfunction}>Guess the culprit</button>
-                        <button onClick={this.logoutUser}>Logout</button>
-                    <p></p>
-                    <p>Make sure you're playing on your desktop, with your screen full size.</p>
-                    <Hatshepsut />
-                </div>
-                
-            </>
-            )
-        } 
-    }
-
-    guessCulpritfunction = (e) => {
-        this.props.history.push('/hatshepsut_frontend/guess')
-    }
-
-    logoutUser = (e) => {
-        this.props.logoutUser()
+          showDream: true,
+        });
+      } else {
+        this.setState({
+          showDream: false,
+        });
       }
-
-    seeIntroduction = (e) => {
-        this.props.history.push('/hatshepsut_frontend/home/setting')
     }
+  };
 
-    continueStory = (e) => {
-        this.props.history.push('/hatshepsut_frontend/home/rooms')
+  toggleItems = (e) => {
+    this.setState({
+      showItems: !this.state.showItems,
+    });
+  };
+
+  renderHome = () => {
+    if (this.props.currentUser) {
+      return (
+        <>
+          <div className="home-content">
+            <Zoom>
+              <h2>Welcome {this.props.currentUser.name}!</h2>
+            </Zoom>
+            <button onClick={this.seeIntroduction}>
+              Re-read the introduction
+            </button>
+            <button onClick={this.continueStory}>
+              Continue where you left off
+            </button>
+            <button onClick={this.guessCulpritfunction}>
+              Guess the culprit
+            </button>
+            <button onClick={this.logoutUser}>Logout</button>
+            <p></p>
+            <p>
+              Make sure you're playing on your desktop, with your screen full
+              size.
+            </p>
+            <Hatshepsut />
+          </div>
+        </>
+      );
     }
+  };
 
-    renderRooms = () => {
-        return (
-            <FirstRoom 
-            toggleItems={this.toggleItems} 
-            zoomState={this.state.showItems} 
-            clickCount={this.state.clickCount}
-            resetClickCount={this.resetClickCount}/>
-        )
-    }
+  guessCulpritfunction = (e) => {
+    this.props.history.push("/hatshepsut_frontend/guess");
+  };
 
-    resetClickCount = () => {
-        this.setState({
-            clickCount: 0
-        })
-    }
+  logoutUser = (e) => {
+    this.props.logoutUser();
+  };
 
-    closeImages = () => {
-        this.setState({
-            showItems: false,
-            clickCount: this.state.clickCount + 1
-        })
+  seeIntroduction = (e) => {
+    this.props.history.push("/hatshepsut_frontend/home/setting");
+  };
 
-    }
+  continueStory = (e) => {
+    this.props.history.push("/hatshepsut_frontend/home/rooms");
+  };
 
-    render() { 
-        return ( 
-            <div>
-                <Switch>
-                    <Route path="/home/setting" component={ Setting } />
-                    <Route path="/home/room" render = { this.renderRooms } />
-                    <Route path="/home/rooms" component = { Rooms } />
-                    <Route path="/guess" component={ GuessCulprit } />
-                    {/* <Route path="/dream" render={ this.renderRandomDream1 } /> */}
-                    <Route render = {this.renderHome} />
-                </Switch>
+  renderRooms = () => {
+    return (
+      <FirstRoom
+        toggleItems={this.toggleItems}
+        zoomState={this.state.showItems}
+        clickCount={this.state.clickCount}
+        resetClickCount={this.resetClickCount}
+      />
+    );
+  };
 
-            { this.state.showItems ?
-            <div className="location-items" >
-                <Items/>
-                <button onClick={this.closeImages} id="close-items-btn">{'\u00D7'}</button>
-            </div>
-            : null
+  resetClickCount = () => {
+    this.setState({
+      clickCount: 0,
+    });
+  };
 
-            }
-            </div>
-         );
-    }
+  closeImages = () => {
+    this.setState({
+      showItems: false,
+      clickCount: this.state.clickCount + 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/hatshepsut_frontend/home/setting" component={Setting} />
+          <Route
+            path="/hatshepsut_frontend/home/room"
+            render={this.renderRooms}
+          />
+          <Route path="/hatshepsut_frontend/home/rooms" component={Rooms} />
+          <Route path="/hatshepsut_frontend/guess" component={GuessCulprit} />
+          {/* <Route path="/dream" render={ this.renderRandomDream1 } /> */}
+          <Route render={this.renderHome} />
+        </Switch>
+
+        {this.state.showItems ? (
+          <div className="location-items">
+            <Items />
+            <button onClick={this.closeImages} id="close-items-btn">
+              {"\u00D7"}
+            </button>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 let mapStateToProps = (state) => {
-    return {
-        currentUser: state.currentUser,
-        allRooms: state.allRooms,
-        userRooms: state.userRooms
-    }
-}
- 
+  return {
+    currentUser: state.currentUser,
+    allRooms: state.allRooms,
+    userRooms: state.userRooms,
+  };
+};
+
 export default connect(mapStateToProps)(withRouter(Home));
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,48 +1,57 @@
-import React from 'react';
+import React from "react";
 
 import Slider from "react-slick";
-import { NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Welcome = () => {
-    var settings = {
-        dots: false,
-        infinite: true,
-        speed: 400,
-        slidesToShow: 1,
-        autoplay: true,
-        pauseOnHover: true,
-        useTransform: true
-        // slidesToScroll: 1
-      };
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 1,
+    autoplay: true,
+    pauseOnHover: true,
+    useTransform: true,
+    // slidesToScroll: 1
+  };
 
-    return ( 
-        <>
-        <div className="welcome-div">
+  return (
+    <>
+      <div className="welcome-div">
         <div className="home-logo-container">
-            <img src="https://res.cloudinary.com/dqtw2xfuf/image/upload/v1595988720/Hatshepsut/Hatshepsut_Mystery_Logo_pres9y.png" alt="main-logo" className="home-logo"/>
+          <img
+            src="https://res.cloudinary.com/dqtw2xfuf/image/upload/v1595988720/Hatshepsut/Hatshepsut_Mystery_Logo_pres9y.png"
+            alt="main-logo"
+            className="home-logo"
+          />
         </div>
         <div className="game-intro-div">
-            <Slider {...settings}>
-            <div >
-                <div className="home-content">
-                    <p>The Hatshepsut Mystery is a one-player murder mystery game, where you can explore the depths of Hatshepsut's Tomb, while playing detective to find out who killed the victim!</p>
-                </div>
+          <Slider {...settings}>
+            <div>
+              <div className="home-content">
+                <p>
+                  The Hatshepsut Mystery is a one-player murder mystery game,
+                  where you can explore the depths of Hatshepsut's Tomb, while
+                  playing detective to find out who killed the victim!
+                </p>
+              </div>
             </div>
             <div className="links-home">
-                <p>
-                <NavLink to="/register">Register</NavLink>    </p>
-                <p>
-                <NavLink to="/login">Login</NavLink>    </p>
+              <p>
+                <NavLink to="/hatshepsut_frontend/register">Register</NavLink>{" "}
+              </p>
+              <p>
+                <NavLink to="/hatshepsut_frontend/login">Login</NavLink>{" "}
+              </p>
             </div>
-            </Slider>
+          </Slider>
         </div>
-        </div>
-            </>
-            );
-}
- 
+      </div>
+    </>
+  );
+};
+
 export default Welcome;
